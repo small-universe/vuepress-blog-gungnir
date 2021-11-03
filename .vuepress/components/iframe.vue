501700235b81fbaea8fb9d70dc678b4478fcaf86
nanci <yunlong.yang@casstime.com>
2021-11-04 01:16:42 +0800

    提交了源码

---
+++ .vuepress/components/iframe.vue
@@ -0,0 +1,25 @@
+<template>
+    <iframe class="video-iframe" width="100%" v-resize="{ log: true }" frameborder="no" scrolling="no" seamless="" allowfullscreen="allowfullscreen" :src="src"></iframe>
+</template>
+
+<script>
+  export default {
+      name:"VueIframe",
+      props: {
+          src: {
+              type: String
+          }
+      },
+      mounted(){
+            function changeVideoIframe(){
+                const video = document.getElementsByClassName('video-iframe');
+                const width = document.getElementsByClassName('video-iframe')[0].scrollWidth;
+                for(let i = 0;i<video.length;i++){
+                    video[i].style.height = width*0.75+'px'
+                }
+            }
+            changeVideoIframe()
+            window.onresize = function(){changeVideoIframe()}
+      }
+  }
+</script>
